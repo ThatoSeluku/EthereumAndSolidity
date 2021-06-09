b078f13ceba44e65ff5aeacbe07ab285f3c1cf90
ThatoSeluku <Thato1000seluku@gmail.com>
2021-06-09 12:54:10 +0200

    Created first smart contract that is able to communicate with blockchain

--- boilerplate-inbox/inbox/compile.js
+++ boilerplate-inbox/inbox/compile.js
@@ -3,9 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
+//Read in the contents of the inbox.sol
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-//Make sure you require compile file and get access to source code
+//Make sure you require compile file and get access to source code from inbox.sol
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
 
--- boilerplate-inbox/inbox/deploy.js
+++ boilerplate-inbox/inbox/deploy.js
@@ -1,1 +1,22 @@
-// deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+//Setup HD Wallet provider
+const provider = new HDWalletProvider(
+'rather monkey range salad under link crouch prosper cram coconut beach assume', 'https://rinkeby.infura.io/v3/409c675de5164908b7b8dffe9573dca8'
+);
+const web3 = new Web3(provider);
+//**********************************Successfully created instance of provider*****************************************/
+
+const deploy = async () => {
+const accounts = await web3.eth.getAccounts();
+console.log('Attempting to deploy from account', accounts[0]);
+const result = await new web3.eth.Contract(JSON.parse(interface))
+.deploy({data: bytecode, arguments: ['Hola']})
+.send({gas: '1000000', from: accounts[0]})
+console.log('Contract deployed to', result.options.address);
+};
+
+
+deploy();
