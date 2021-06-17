21a2f09601aaa79ac407d8fbd0fa85ed185ed865
ThatoSeluku <Thato1000seluku@gmail.com>
2021-06-17 20:25:29 +0200

    Finished function modifiers tests

--- Project2/lottery-boilerplate/lottery/test/Lottery.test.js
+++ Project2/lottery-boilerplate/lottery/test/Lottery.test.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
+const { send } = require("process");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
@@ -68,5 +69,28 @@ assert.strictEqual(accounts[2], players[2]);
 assert.strictEqual(accounts[3], players[3]);
 
 assert.strictEqual(4, players.length);
+  });
+
+  it('requires a minimum amount of ether to enter', async()=>{
+try{
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: 200 
+    });
+    assert(false);
+  } catch(err){
+    assert(err);
+  }
+  })
+
+  it('Only manager can call pickwinner', async()=>{
+    try{
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      });
+      assert(false);
+    }catch(err){
+      assert(err);
+    }
   })
 });
