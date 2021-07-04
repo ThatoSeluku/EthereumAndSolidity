3b278631adf7c92aa842d7358c2a87b8bcb94d00
ThatoSeluku <Thato1000seluku@gmail.com>
2021-07-04 16:06:05 +0200

    Ran tests

--- Project3/boilerplate-kickstart/kickstart/test/Campaign.test.js
+++ Project3/boilerplate-kickstart/kickstart/test/Campaign.test.js
@@ -12,8 +12,59 @@ let campaignAddress;
 let campaign;
 
 beforeEach(async()=>{
-    accounts = await web3.eth.getAccounts();
-    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+accounts = await web3.eth.getAccounts();
+factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
 .deploy({data: compiledFactory.bytecode})
 .send({from: accounts[0], gas: '1000000'});
-})
+
+await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000'  
+});
+
+[campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+campaign =await new web3.eth.Contract(
+JSON.parse(compiledCampaign.interface),
+campaignAddress 
+);
+});
+
+describe('Campaigns', ()=>{
+it('deploys a factory and campaign', ()=>{
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+});
+it('Marks the caller as campaign manager', async () =>{
+const manager = await campaign.methods.manager().call();
+assert.strictEqual(accounts[0], manager);
+});
+it("Allows people to contribute money and marks them as approvers", async ()=> {
+    await campaign.methods.contribute().send({
+        value: '200',
+        from: accounts[1]
+    }); 
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+assert(isContributor);
+
+});
+it('Requires a minimum contribution', async()=>{
+ try{
+    await campaign.methods.contribute().send({
+        value: '99',  
+        from: accounts[1]
+    });
+    assert(false);
+ }catch(err){
+assert(err);
+ }
+});
+it('Allows a manager to make a payment request', async()=>{
+    await campaign.methods.createRequest('Request 1', '100', accounts[1])
+    .send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+    const request = await campaign.methods.requests(0).call();
+assert.strictEqual('Request 1', request.description);
+   });
+});
