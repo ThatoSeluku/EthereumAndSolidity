395f3c83a5850470aa4d1acc7ba95a4bd941e29f
ThatoSeluku <Thato1000seluku@gmail.com>
2021-07-31 22:27:01 +0200

    Address shows on home page, changed server to custom

--- Project3/boilerplate-kickstart/kickstart/pages/campaigns/new.js
+++ Project3/boilerplate-kickstart/kickstart/pages/campaigns/new.js
@@ -4,24 +4,31 @@ import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import {Link, Router} from '../../routes';
+
 
 class CampaignNew extends Component{
    state={
 minimumContribution: '',
-errorMessage: ''
+errorMessage: '', 
+loading: false
    };
 
    onSubmit= async (event) =>{
     event.preventDefault();
+    this.setState({loading: true, errorMessage: ''});
 try{
 const accounts = await web3.eth.getAccounts();
 await factory.methods
 .createCampaign(this.state.minimumContribution).send({
     from: accounts[0],
   });
+
+  Router.pushRoute('/');
 }catch(error){
 this.setState({errorMessage: error.message});
 }
+this.setState({loading: false})
 };
     render(){
         return(
@@ -34,7 +41,7 @@ this.setState({errorMessage: error.message});
 onChange={event=>this.setState({minimumContribution: event.target.value})}></Input>
                 </Form.Field>
           <Message error header="Oops!" content={this.state.errorMessage}/>
-            <Button primary>Create</Button>
+            <Button loading={this.state.loading} primary>Create</Button>
             </Form>
         </Layout>
         );
